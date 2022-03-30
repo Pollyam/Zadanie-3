53051eca40d812a9bba74d6bc2eaa8075bce4987
Pollyam <101092550+Pollyam@users.noreply.github.com>
2022-03-30 09:34:10 +0300

    Add files via upload

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.NewFolder1;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void avtoriz_Click(object sender, RoutedEventArgs e)
+        {
+            if ((login.Text == "admin") && (parol.Text == "admin"))
+            {
+                Klient klient = new Klient();
+                klient.Show();
+            }
+            else MessageBox.Show("Неверный пароль или логин");
+        }
+    }
+}
